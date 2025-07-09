17921e4968430ac5f995712d3bd83b4ad51c2c89
Viktor <viktor.osadsky@gmail.com>
2025-07-09 14:02:04 -0400

    Initial commit

---
+++ bankaccount.py
@@ -0,0 +1,20 @@
+class Bankaccount:
+    def __init__(self, balance, name, accountnum):
+        self.balance = balance
+        self.name = name
+        self.accountnum = accountnum
+
+    def withdraw(amt):
+        balance -= amt
+
+    def deposit(amt):
+        balance += amt
+
+    def balance():
+        print(balance)
+
+
+bc = Bankaccount(1000, "Viktor", 100)
+bc.withdraw(20)
+bc.deposit(30)
+bc.blance()
